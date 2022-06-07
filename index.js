a06725f7b43dcffac559ee827a1293ce72b73f11
iblas <ibrahimiblas@gmail.com>
2022-06-07 22:02:09 +0100

    backend_with_frontend

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ let notes = require("./noteData");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
